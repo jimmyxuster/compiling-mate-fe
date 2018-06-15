def1a6ac25ba829432e1820844ed13db028f8159
Unknown <756762961@qq.com>
2018-06-15 14:03:10 +0800

    about page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ConceptTree from './component/ConceptTree/ConceptTree'
 import LeftRightLayout from './component/LeftRightLayout/LeftRightLayout'
 import LexPage from './component/LexPage/LexPage';
 import CoLangPage from './component/CoLang/CoLangPage';
+import AboutPage from './component/AboutPage/AboutPage';
 const {Header} = Layout
 
 class App extends Component {
@@ -49,6 +50,7 @@ class App extends Component {
                     <Route path="/algorithm" component={LeftRightLayout}/>
                     <Route path="/lex" component={LexPage}/>
                     <Route path="/colang" component={CoLangPage}/>
+                    <Route path="/about" component={AboutPage}/>
                 </Layout>
             </Router>
         )
