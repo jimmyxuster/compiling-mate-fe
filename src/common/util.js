6f2ece26f6f3aa8231e83ae9cb9feac6d61500c4
jimmyxuster <jimmyxuster@outlook.com>
2018-06-20 12:59:05 +0800

    fix slr bug

--- src/common/util.js
+++ src/common/util.js
@@ -73,9 +73,6 @@ function calcNodePositions(nodes) {
 }
 
 function parseProduction(node) {
-    if (!('productionLeft' in node) || !('productionRight' in node) || !('left' in node) || !('right' in node)) {
-        throw new TypeError('productionLeft and productionRight must be attributes of node!');
-    }
     const left = node.productionLeft || node.left;
     const right = node.productionRight || node.right;
     let result = '';
