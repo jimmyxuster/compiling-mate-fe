f63f76305b7951e1756a07f8c885add1db8e5706
智勇 王 <756762961@qq.com>
2018-06-21 20:34:50 +0800

    add a modern editor for colang

--- src/component/CoLang/CoLangPage.js
+++ src/component/CoLang/CoLangPage.js
@@ -3,63 +3,90 @@ import {Row, Col, Input, Button, Card} from 'antd';
 import api from '../../service/api';
 import TokenList from './Token/TokenList';
 import './CoLangPage.css';
+import * as CodeMirror from 'codemirror';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/mode/javascript/javascript.js';
 const {TextArea} = Input;
 
 class CoLangPage extends Component {
-  constructor () {
-    super ();
+  constructor() {
+    super();
     this.state = {
       code: 'print "hello world";',
       output: [],
       tokens: [],
+      codeMirror: null
     };
   }
 
   onCodeChange = e => {
     const {value} = e.target;
-    this.setState ({code: value});
+    this.setState({code: value});
   };
 
   run = () => {
-    api.runCoLangCode ({code: this.state.code}).then (res => {
-      if (res.success) {
-        let {data} = res;
-        this.setState ({
-          output: data.output,
-          tokens: data.tokens,
-        });
-      }
-    });
+    api
+      .runCoLangCode({code: this.state.codeMirror.getValue()})
+      .then(res => {
+        if (res.success) {
+          let {data} = res;
+          this.setState({output: data.output, tokens: data.tokens});
+        }
+      });
   };
 
-  render () {
+  componentDidMount() {
+    this.setState({
+      codeMirror: CodeMirror(document.getElementById ('colang_editor'), {
+        value: this.state.code,
+        mode:  "javascript",
+        lineNumbers: true,
+        theme: 'dracula'
+      })
+    });
+  }
+
+  render() {
     return (
       <div className="colang-page-container">
+        <div id="colang_editor"></div>
         <Row gutter={16}>
           <Col span={6} offset={3}>
             <Card title="Code Editor" bordered={false}>
               <TextArea
                 placeholder="Enter your colang code here"
-                autosize={{minRows: 5}}
+                autosize={{
+                minRows: 5
+              }}
                 onChange={this.onCodeChange}
-                defaultValue={this.state.code}
-              />
-              <Button type="primary" style={{margin: '8px 0'}} onClick={this.run}>
+                defaultValue={this.state.code}/>
+              <Button
+                type="primary"
+                style={{
+                margin: '8px 0'
+              }}
+                onClick={this.run}>
                 Run
               </Button>
             </Card>
           </Col>
           <Col span={6}>
             <Card title="Token List" bordered={false}>
-              <TokenList tokens={this.state.tokens} />
+              <TokenList tokens={this.state.tokens}/>
             </Card>
           </Col>
           <Col span={6}>
             <Card title="Output" bordered={false}>
               <div className="output-container">
-                {this.state.output.map ((val, index) => (
-                  <p key={index} style={{margin: 0}}>{val}</p>
-                ))}
+                {this
+                  .state
+                  .output
+                  .map((val, index) => (
+                    <p key={index} style={{
+                      margin: 0
+                    }}>{val}</p>
+                  ))}
               </div>
             </Card>
           </Col>
