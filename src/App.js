3f1761def14e0c348299df693fab15fb4409aaa2
jimmyxuster <jimmyxuster@outlook.com>
2018-06-24 18:07:07 +0800

    type checking

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ConceptTree from './component/ConceptTree/ConceptTree'
 import LeftRightLayout from './component/LeftRightLayout/LeftRightLayout'
 import LexPage from './component/LexPage/LexPage';
 import CoLangPage from './component/CoLang/CoLangPage';
+import Semantic from './component/semantic/Semantic'
 const {Header} = Layout
 
 class App extends Component {
@@ -40,6 +41,7 @@ class App extends Component {
                         >
                             <Menu.Item key="/concepts"><Link to="/concepts">知识图谱</Link></Menu.Item>
                             <Menu.Item key="/algorithm"><Link to="/algorithm">算法演示</Link></Menu.Item>
+                            <Menu.Item key="/type-checking"><Link to="/type-checking">类型检查</Link></Menu.Item>
                             <Menu.Item key="/colang"><Link to="/colang">Co语言</Link></Menu.Item>
                         </Menu>
                     </Header>
@@ -47,6 +49,7 @@ class App extends Component {
                     <Route path="/concepts" component={ConceptTree}/>
                     <Route path="/algorithm" component={LeftRightLayout}/>
                     <Route path="/lex" component={LexPage}/>
+                    <Route path="/type-checking" component={Semantic}/>
                     <Route path="/colang" component={CoLangPage}/>
                 </Layout>
             </Router>
